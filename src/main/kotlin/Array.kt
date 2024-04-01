5615f000ffdfad45897e6ef42f7c64468f43f575
priya Kumari <priyakum1071@gmail.com>
2024-04-01 14:58:44 +0530

    Program

--- src/main/kotlin/Array.kt
+++ src/main/kotlin/Array.kt
@@ -1,9 +1,15 @@
 fun main(){
-    val cars = arrayOf("volvo","BMW","Ford","Mazda")
+      val cars = arrayOf("volvo","BMW","Ford","Mazda")
     if("BMW" in cars){
         print(" This car is avilable")
     }
     else{
-        print(" This car is not avilable")
+        println(" This car is not avilable")
     }
+    println(" ")
+
+    val arr= arrayOf(1,2,3,4,5)
+    for(i in arr )
+
+    println( i)
 }
---
+++ src/main/kotlin/NumberOfElement.kt
@@ -0,0 +1,5 @@
+fun main(){
+    val arr = arrayOf(20,30,45,64,93,85,63)
+
+   println("Number of Element in array: ${arr.size}")
+}
